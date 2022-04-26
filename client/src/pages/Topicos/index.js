0c8d9aca320ef3cad59647aa2741dc71aa89b7ff
Wesley Toledo <wesley.toledo.hexatech@gmail.com>
2022-04-26 13:34:44 -0300

    .

--- client/src/pages/Topicos/index.js
+++ client/src/pages/Topicos/index.js
@@ -14,6 +14,7 @@ function TopicosPage() {
     {
       name: "Sólidos Geométricos: Poliedros",
       link: "/topicos/poliedros",
+      
     }, 
     {
       name: "Sólidos Geométricos: Corpos Redondos",
